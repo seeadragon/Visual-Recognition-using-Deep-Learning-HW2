e8dc5c0ca470c7de4fe25545bce06bd9935b4639
seadragon <magary1025@gmail.com>
2025-04-16 20:31:12 +0800

    model pth

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ if __name__ == '__main__':
         'pretrained': True,
         'num_epochs': 10,
         'batch_size': 2,
-        'learning_rate': 4e-5, #v1 - v3 1e-4 #v4-v5 5e-5 # v5 2.5e-5 # v6 4e-5
+        'learning_rate': 4e-5,
         'weight_decay': 5e-5,
         'optimizer': optim.AdamW,
         'log_dir': 'log/visualization',
     }
     multiprocessing.freeze_support()
     model = FasterRCNN(config)
-    #model.load_model("log\\faster_rcnn_resnet50_fpn_v6\\best_model.pth")
+    #model.load_model("model.pth")
     model.train()
     #model.eval()
     #model.visual()
